80f40c4863fed77369922a4d410cf787137a97d6
Apurv Chavda <apurvchavda15@gmail.com>
2026-03-17 00:35:49 +0530

    make mongoDb connection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-import e, { json } from "express";
+import { configDotenv } from "dotenv";
+import express, { json } from "express";
+import mongoose from "mongoose";
+configDotenv()
 
-const app = e()
-
+const app = express()
 app.use(json())
+
+await mongoose.connect(process.env.MONGO_URI).then(() => console.log("mongo db connection get succesfull"))
 
 app.get("/",(req,res)=>{
       res.json("server is working fine !!")
 })
 
-app.listen(3000,console.log("server running on Port 3000"))
+const PORT = process.env.PORT
+app.listen(PORT,() => console.log("server running on Port 3000"))
